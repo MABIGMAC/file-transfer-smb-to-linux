f1cf375ee23a89026c8389fb2a019d9a1864906b
McBigMac <jy1996@hotmail.com>
2025-06-23 12:24:42 +0800

    chore: update to verify by downloading

--- scripts/lib/utils.py
+++ scripts/lib/utils.py
@@ -1,5 +1,5 @@
 import hashlib
-
+import tempfile
 def md5_checksum(file_path):
     """Return the hex MD5 checksum of a local file."""
     hash_md5 = hashlib.md5()
@@ -11,3 +11,14 @@ def md5_checksum(file_path):
 def convert_to_s3_key(file_path):
     # Normalize to "folder/file" format, remove SMB share root
     return file_path.replace("\\", "/").lstrip("/")
+
+def verify_upload(bucket, s3_key, local_file):
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        print(f"🔄 Downloading '{s3_key}' from S3 to temporary file...")
+        bucket.download_file(s3_key, tmp_file.name)
+        downloaded_md5 = md5_checksum(tmp_file.name)
+        original_md5 = md5_checksum(local_file)
+
+    print(f"🔎 Local MD5     : {original_md5}")
+    print(f"🔎 Downloaded MD5: {downloaded_md5}")
+    return downloaded_md5 == original_md5
--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,12 @@
 import os
 from lib.env_var import *
-from lib.utils import md5_checksum
+from lib.utils import verify_upload
 from lib.s3_client import get_s3_resource, ensure_bucket_exists, list_bucket_objects, upload_file_with_validation
 
-# === Write test file ===
-os.makedirs(os.path.dirname(LOCAL_FILE_PATH), exist_ok=True)
-with open(LOCAL_FILE_PATH, 'w') as f:
-    f.write('Hello from Python!')
+# # === Write test file ===
+# os.makedirs(os.path.dirname(LOCAL_FILE_PATH), exist_ok=True)
+# with open(LOCAL_FILE_PATH, 'w') as f:
+#     f.write('Hello from Python!')
 
 # Connect to S3
 s3 = get_s3_resource()
@@ -19,14 +19,14 @@ bucket = ensure_bucket_exists(s3, BUCKET_NAME)
 upload_file_with_validation(bucket, LOCAL_FILE_PATH, S3_KEY)
 print(f"📤 Uploaded '{LOCAL_FILE_PATH}' to '{S3_KEY}'")
 
-# Verify with checksum
-local_md5 = md5_checksum(LOCAL_FILE_PATH)
-etag = s3.Object(BUCKET_NAME, S3_KEY).e_tag.strip('"')
+# # Verify with checksum
+# local_md5 = md5_checksum(LOCAL_FILE_PATH)
+# etag = s3.Object(BUCKET_NAME, S3_KEY).e_tag.strip('"')
+# print(f"🔎 Local MD5: {local_md5}")
+# print(f"🔎 S3 ETag : {etag}")
+verifyed = verify_upload(bucket, S3_KEY, LOCAL_FILE_PATH)
 
-print(f"🔎 Local MD5: {local_md5}")
-print(f"🔎 S3 ETag : {etag}")
-
-if local_md5 == etag:
+if verifyed:
     print("✅ File upload verified successfully!")
 else:
     print("❌ Upload verification failed (checksum mismatch)")
