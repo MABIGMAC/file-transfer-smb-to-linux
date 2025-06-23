26ff0624e39dc9fa901f6d873c86d70dc3bd712a
McBigMac <jy1996@hotmail.com>
2025-06-23 17:42:59 +0800

    chore: use md5 and etag to view differences

--- scripts/lib/utils.py
+++ scripts/lib/utils.py
@@ -1,5 +1,7 @@
 import hashlib
 import tempfile
+import time
+
 def md5_checksum(file_path):
     """Return the hex MD5 checksum of a local file."""
     hash_md5 = hashlib.md5()
@@ -12,10 +14,32 @@ def convert_to_s3_key(file_path):
     # Normalize to "folder/file" format, remove SMB share root
     return file_path.replace("\\", "/").lstrip("/")
 
+import hashlib
+
+def calculate_s3_multipart_etag(file_path, part_size=8 * 1024 * 1024):
+    md5s = []
+    with open(file_path, 'rb') as f:
+        while chunk := f.read(part_size):
+            md5s.append(hashlib.md5(chunk).digest())
+
+    if len(md5s) == 1:
+        return hashlib.md5(md5s[0]).hexdigest()
+    else:
+        joined_md5 = b''.join(md5s)
+        final_etag = hashlib.md5(joined_md5).hexdigest()
+        return f"{final_etag}-{len(md5s)}"
+
+
 def verify_upload(bucket, s3_key, local_file):
     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
         print(f"🔄 Downloading '{s3_key}' from S3 to temporary file...")
+        start_time = time.time()  # ⏱️ Start timer
+
         bucket.download_file(s3_key, tmp_file.name)
+
+        end_time = time.time()    # ⏱️ End timer
+        duration = end_time - start_time
+        print(f"⏱️ Download completed in {duration:.2f} seconds")
         downloaded_md5 = md5_checksum(tmp_file.name)
         original_md5 = md5_checksum(local_file)
 
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,6 @@
 import os
 from lib.env_var import *
-from lib.utils import verify_upload
+from lib.utils import verify_upload, md5_checksum, calculate_s3_multipart_etag
 from lib.s3_client import get_s3_resource, ensure_bucket_exists, list_bucket_objects, upload_file_with_validation, put_object_with_validation
 
 # # === Write test file ===
@@ -20,13 +20,13 @@ bucket = ensure_bucket_exists(s3, BUCKET_NAME)
 put_object_with_validation(bucket, LOCAL_FILE_PATH, S3_KEY)
 
 # # Verify with checksum
-# local_md5 = md5_checksum(LOCAL_FILE_PATH)
-# etag = s3.Object(BUCKET_NAME, S3_KEY).e_tag.strip('"')
-# print(f"🔎 Local MD5: {local_md5}")
-# print(f"🔎 S3 ETag : {etag}")
-verifyed = verify_upload(bucket, S3_KEY, LOCAL_FILE_PATH)
+local_md5 = md5_checksum(LOCAL_FILE_PATH)
+etag = s3.Object(BUCKET_NAME, S3_KEY).e_tag.strip('"')
+print(f"🔎 Local MD5: {local_md5}")
+print(f"🔎 S3 ETag : {etag}")
+# verifyed = verify_upload(bucket, S3_KEY, LOCAL_FILE_PATH)
 
-if verifyed:
+if local_md5 == etag:
     print("✅ File upload verified successfully!")
 else:
     print("❌ Upload verification failed (checksum mismatch)")
